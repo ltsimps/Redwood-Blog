f651409e74ccb6e96fcd33a2af40175cf70c1c2b
dev <dev@devs-MacBook-Pro.local>
2022-11-07 09:29:28 -0500

    Redwood Blog

---
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -0,0 +1,43 @@
+import { useAuth } from '@redwoodjs/auth'
+import { Link, routes } from '@redwoodjs/router'
+
+const BlogLayout = ({ children }) => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
+  return (
+    <>
+      <header className="flex-between">
+        <div>
+          <h1>
+            <Link to={routes.home()}> Redwood Blog </Link>
+          </h1>
+          {isAuthenticated ? (
+            <div>
+              <span> Logged in as {currentUser.email} </span>{' '}
+              <button type="button" onClick={logOut}>
+                {' '}
+                Log Out{' '}
+              </button>
+            </div>
+          ) : (
+            <Link to={routes.login()}>Login</Link>
+          )}
+        </div>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.home()}>Home</Link>`<li></li>
+              <Link to={routes.about()}>About</Link>`
+            </li>
+            <li>
+              <Link to={routes.contact()}>Contact Page</Link>`
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
+  )
+}
+
+export default BlogLayout
---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,15 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <p>
+        This site was created to demonstrate my mastery of RedwoodJS. Look at
+        all the cool stuff I can do!
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
